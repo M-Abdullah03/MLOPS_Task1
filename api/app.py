3cd081713db77cad8292e1a09af8f121440c7daf
M-Abdullah03 <m18ab03@gmail.com>
2025-02-18 13:29:27 +0500

    Vercel.json added

--- api/app.py
+++ api/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return "Deploying Flask App at Vercel"
 
+@app.route('/about')
+def about():
+    return 'About'
+
 if __name__ == "__main__":
     app.run(debug=True)
 
