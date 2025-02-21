6bf56927c3b9a9383c0b44211bc6ce0e6f40653a
Bored-Student <i211215@nu.edu.pk>
2025-02-21 12:54:48 +0500

    Abd

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deploying Flask App Vercelrough git actions"
+    return "Deploying Flask Ap Vercelrough git actions"
 
 @app.route('/about')
 def about():
